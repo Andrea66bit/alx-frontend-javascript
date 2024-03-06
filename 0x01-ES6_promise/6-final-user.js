dcb4b7e68cdfd367df9a3db11f93995a72fcc911
Andrea66bit <njabuloandrea@gmail.com>
2024-03-06 12:00:45 +0000

    programme is fun alone

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,18 +5,18 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUp = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUp, upload].then((value) => {
+  return Promise.allSettled([signUp, upload]).then((values) => {
     const result = [];
-   value.forEach((val) => {
-     if (val.status === 'fulfilled') {
-       result.push({ val.status, value: val.value });
-     } else {
-       result.push({
-         status: val status,
-         value: 'Error: ${val.reason.message}',
-       });
-     }
-   });
-   return result;
- });
+    values.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        result.push({ status: val.status, value: val.value });
+      } else {
+        result.push({
+          status: val.status,
+          value: `Error: ${val.reason.message}`,
+        });
+      }
+    });
+    return result;
+  });
 }
